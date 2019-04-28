fedfac77cbd161246485b125a9ef9daa9838978d
jiangqiang <jiangq.leaves@gmail.com>
2019-04-28 11:44:46 +0800

    Add types

---
+++ src/index.d.ts
@@ -0,0 +1,3 @@
+export * from './index';
+import * as Types from './types';
+export { Types };
---
+++ src/types.ts
@@ -0,0 +1,15 @@
+// exported types
+export { IEventEmitter, RouteActionType, RouteEventType, ViewEventType, IRouteRecord } from './interface/common';
+export { IDriverEventMap, IRouterDriver, RouteDriverEventType } from './interface/driver';
+export { IMatchedRoute, IRouteManager } from './interface/routeManager';
+export {
+  IRouter,
+  IRouterEventMap,
+  ILocation,
+  IPathnameLocation,
+  INameLocation,
+  IRouteConfig,
+  IRouteInfo,
+  IRoute,
+  INavigateOption
+} from './interface/router';
