79c6d5cdf74c09e651e6b44de0f0974defe2e6eb
jiangqiang <jiangq.leaves@gmail.com>
2019-05-21 17:08:26 +0800

    Supported lifecycle hooks in child components

--- src/utils/invokeHook.ts
+++ src/utils/invokeHook.ts
@@ -2,8 +2,9 @@ import Vue from 'vue';
 import { ViewActionType } from '../interface/common';
 
 export default function invokeHook(component: Vue, hookName: ViewActionType) {
-   const hook = component.$options[hookName];
-   if (hook) {
+  const hook = component.$options[hookName];
+  if (hook) {
     hook.call(component);
-   }
+  }
+  component.$children.forEach(child => invokeHook(child, hookName));
 }
