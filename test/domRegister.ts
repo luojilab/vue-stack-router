49864a8af832e0c25b261fccb8799dc0ed9169f3
jiangqiang <jiangq.leaves@gmail.com>
2019-07-31 17:51:43 +0800

    test: chanage dom register to ts from js

---
+++ test/domRegister.ts
@@ -0,0 +1,15 @@
+import { JSDOM } from 'jsdom';
+
+export function register() {
+  const dom = new JSDOM(``, {
+    url: 'https://example.org/',
+    referrer: 'https://example.com/',
+    contentType: 'text/html',
+    includeNodeLocations: true,
+    storageQuota: 10000000
+  });
+  globalThis.window = dom.window;
+}
+export function cancel() {
+  delete globalThis.window;
+}
