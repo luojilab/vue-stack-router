f4946ae8234989e8b10e0fa6832f470a32cf5383
jiangqiang <jiangq.leaves@gmail.com>
2019-08-05 18:30:15 +0800

    feat(driver): add server driver

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import RouterView from './component/RouterView';
 import BrowserDriver from './driver/browser/BrowserDriver';
+import ServerDriver from './driver/browser/ServerDriver';
 import installer from './lib/installer';
 import Router from './lib/Router';
 
 import * as Types from './types';
 
-export { Router, BrowserDriver, RouterView, installer, Types };
+export { Router, BrowserDriver, ServerDriver, RouterView, installer, Types };
