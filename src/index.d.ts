b3dc45c69a5c45ed10df38ea674652b24c70448f
jiangqiang <jiangq.leaves@gmail.com>
2019-09-10 19:46:33 +0800

    refactor: move types to index file

--- src/index.d.ts
+++
@@ -1,9 +0,0 @@
-import BrowserDriver from './driver/browser/BrowserDriver';
-import ServerDriver from './driver/browser/ServerDriver';
-import installer from './lib/installer';
-import Router from './lib/Router';
-import RouterView from './view/RouterView';
-
-import * as Types from './types';
-
-export { Router, BrowserDriver, RouterView, installer, Types, ServerDriver };
