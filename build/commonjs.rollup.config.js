c62d9627b4cd823daef7d005b4e110d155c1cfbf
jiangqiang <jiangq.leaves@gmail.com>
2020-07-27 14:38:03 +0800

    style(build): format

--- build/commonjs.rollup.config.js
+++ build/commonjs.rollup.config.js
@@ -1,17 +1,17 @@
-import typescript from "rollup-plugin-typescript2";
+import typescript from 'rollup-plugin-typescript2';
 export default {
-  input: "./src/index.ts",
+  input: './src/index.ts',
   output: {
-    file: "dist/vue-stack-router.js",
-    format: "cjs",
-    name: "VueStackRouter",
-    exports: "named"
+    file: 'dist/vue-stack-router.js',
+    format: 'cjs',
+    name: 'VueStackRouter',
+    exports: 'named',
   },
   plugins: [
     typescript({
-      tsconfig: "./tsconfig.json",
-      tsconfigOverride: { compilerOptions: { target: "es5" } }
-    })
+      tsconfig: './tsconfig.json',
+      tsconfigOverride: { compilerOptions: { target: 'es5' } },
+    }),
   ],
-  external: ["vue"]
+  external: ['vue'],
 };
--- build/esm.rollup.config.js
+++ build/esm.rollup.config.js
@@ -1,16 +1,16 @@
-import typescript from "rollup-plugin-typescript2";
+import typescript from 'rollup-plugin-typescript2';
 export default {
-  input: "./src/index.ts",
+  input: './src/index.ts',
   output: {
-    file: "dist/vue-stack-router.esm.js",
-    format: "es",
-    name: "VueStackRouter"
+    file: 'dist/vue-stack-router.esm.js',
+    format: 'es',
+    name: 'VueStackRouter',
   },
   plugins: [
     typescript({
-      tsconfig: "./tsconfig.json",
-      tsconfigOverride: { compilerOptions: { target: "es5" } }
-    })
+      tsconfig: './tsconfig.json',
+      tsconfigOverride: { compilerOptions: { target: 'es5' } },
+    }),
   ],
-  external: ["vue"]
+  external: ['vue'],
 };
